ae429cc130cdbe3861fa6dbfb1bbdea470be0293
tommycoleman87 <tommiilee@gmail.com>
2019-07-26 14:21:56 -0400

    added search functionality

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,28 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
-import CharacterCard from './CharacterCard';
-    function CharacterList() {
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
+import SearchForm from './SearchForm';
+function CharacterList() {
   // TODO: Add useState to track data from useEffect
-    const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
+  const [searchChar, setSearchChar] = useState();
+
+  const onSearch = value => {
+    setSearchChar(value)
+  };
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    Axios.get('https://rickandmortyapi.com/api/character/')
-    .then(data => {
-      console.log(data.data.results);
-      setCharacters(data.data.results);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }, [])
+    if (searchChar) {
+      Axios.get(`https://rickandmortyapi.com/api/character/?name=${searchChar}`)
+        .then(data => {
+          console.log(data);
+          setCharacters(data.data.results)
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    } else {
+      Axios.get("https://rickandmortyapi.com/api/character/")
+        .then(data => {
+          console.log(data.data.results);
+          setCharacters(data.data.results);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, [searchChar]);
 
-  return <section className='character-list grid-view'>
-    {characters.map(character => {
-      return <CharacterCard character={character} key={character.url}/>
-    })}
+  return (<div><SearchForm onSearch={onSearch}/>
+    <section className="character-list grid-view">
+     
+      {characters.map(character => {
+        return <CharacterCard character={character} key={character.url} />;
+      })}
     </section>
-
+    </div>
+  );
 }
 
 export default CharacterList;
